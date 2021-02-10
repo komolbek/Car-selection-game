42c0bbb01c8fbe89589480b19c109fddb188fa90
Kamol <rusmirolub@gmail.com>
2021-02-10 13:52:35 +0000

    [Android-master] Implemented go back functionality for all activities

--- app/src/main/java/com/example/w1754980cargame/CarImageIdentifyActivity.java
+++ app/src/main/java/com/example/w1754980cargame/CarImageIdentifyActivity.java
@@ -3,12 +3,34 @@ package com.example.w1754980cargame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CarImageIdentifyActivity extends AppCompatActivity {
+
+    Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_car_image_identify);
+        setContentView(R.layout.activity_car_make_identify);
+
+        setupButtons();
+        setupButtonListeners();
+    }
+
+    // Private methods
+
+    private void setupButtons() {
+        backButton = findViewById(R.id.carImageBackButton);
+    }
+
+    private void setupButtonListeners() {
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
